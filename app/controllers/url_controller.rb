10347bfdf2c33b20661016c093c9b22059b9a359
Mark Muskardin <mmuskardin@railsteam7.local>
2010-01-26 17:43:42 -0500

    [GJ/MM] Displaying overall scores on main page

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -2,13 +2,25 @@ class UrlController < ApplicationController
   
   def index
     @urls = Url.find(:all)
+    @grades = {}
+    
+    @urls.each do |u|
+      y = u.yslow2s.find(:last)
+      p = u.page_speeds.find(:last)
+      
+      @grades[:url] = u
+      @grades[:url][:yslow] = y.o
+      unless p == nil
+        @grades[:url][:page_speed] = p.o
+      end
+    end
+    
   end
 
   def show
     @url = Url.find(params[:id])
-    
+    @url_name = @url.url
     @yslow_scores = @url.yslow2s.find(:all)
-    
   end
 
 end
