eef97d8b87993853449c47ec6a45bb75ad847776
Mark Muskardin <mmuskardin@railsteam7.local>
2010-01-27 15:26:52 -0500

    [GJ/MM] New data structure for yslow/page speed grades

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -2,13 +2,13 @@ class UrlController < ApplicationController
   
   def index
     @urls = Url.find(:all)
-    @grades = Hash.new { |hash,key| hash[key] = { } }
+    @grades = Hash.new { |hash,key| hash[key] = [] }
     
     @urls.each do |u|
       y = u.yslow2s.find(:last)
       p = u.page_speeds.find(:last)
-      @grades[u].store(:yslow, y.o) unless y == nil
-      @grades[u].store(:page_speed, p.o) unless p == nil
+      @grades[u][0] = (y==nil) ? 'N/A' : y.o #.store(:yslow, y.o) unless y == nil
+      @grades[u][1] = (p==nil) ? 'N/A' : p.o #.store(:page_speed, p.o) unless p == nil
     end 
   end
 
