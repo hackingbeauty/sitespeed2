cc39925cbd960207ad40d51129327aec815705c1
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-03-26 00:49:22 -0400

    update

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'will_paginate', :version => '2.2.2'
-  config.gem 'haml', :version => '2.2.21'
+  config.gem 'haml', :version => '>=2.2.21'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
