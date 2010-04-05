29c0d9928946592e0e8d9c2c48f6bdd51a2c4f24
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-04-05 14:09:22 -0400

    heroku error on ORDER BY sql clause

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   def fastest_sites
-    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10, :order => "o DESC")
+    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10)
     top_ten_fastest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{td.url.yslow2s.last.o}</td></tr>"}
     top_ten_fastest << "</table>"   
   end
