ca9fe1c89550d5bb2981e3e504ba4ce374a478c5
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-04-05 14:10:42 -0400

    removed ASC order for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   end
 
   def slowest_sites
-    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10, :order => "o ASC")
+    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10)
     top_ten_slowest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{td.url.yslow2s.last.o}</td></tr>"}
     top_ten_slowest << "</table>"   
   end
