88df61afa7c506cf3518f4238af7957092a8df57
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-04-13 16:23:41 -0400

    tweaking top 10 fastest/slowest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@
 module ApplicationHelper
 
   def fastest_sites
-    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10)
+    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 9)
     top_ten_fastest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{td.url.yslow2s.last.o}</td></tr>"}
     top_ten_fastest << "</table>"   
   end
 
   def slowest_sites
-    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 10)
+    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 9)
     top_ten_slowest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{td.url.yslow2s.last.o}</td></tr>"}
     top_ten_slowest << "</table>"   
   end
