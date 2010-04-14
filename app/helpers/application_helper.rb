0978bd0e275cd503ea2cc94c06c3b0b5fc2081bb
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-04-13 21:01:57 -0400

    tweaking the top 10 slow 10 sql query - heroku uses postgres

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,15 @@
 module ApplicationHelper
 
   def fastest_sites
-    yslow_grades = Yslow2.find(:all, :order => 'o DESC', :select => 'DISTINCT url_id',  :limit => 9)
+    # yslow_grades = Yslow2.find(:all, :order => 'o DESC', :select => 'DISTINCT url_id',  :limit => 9)
+    yslow_grades = Yslow2.find_by_sql(["SELECT o, url_id FROM yslow2s ORDER BY o desc LIMIT 9"])
     top_ten_fastest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{link_to td.url.yslow2s.last.o, td.url}</td></tr>"}
     top_ten_fastest << "</table>"   
   end
 
   def slowest_sites
-    yslow_grades = Yslow2.find(:all, :select => 'DISTINCT url_id', :limit => 9)
+    # yslow_grades = Yslow2.find(:all, :order => 'o ASC', :select => 'DISTINCT url_id', :limit => 9)
+    yslow_grades = Yslow2.find_by_sql(["SELECT o, url_id FROM yslow2s ORDER BY o asc LIMIT 9"])
     top_ten_slowest = yslow_grades.inject("<table><thead><th>Rank</th><th>Url</th><th>Yslow</th></thead>") {|table, td| table + "<tr><td class='center'>#{td.url.country_rank}</td><td>#{td.url.url_name}</td><td class='center'>#{link_to td.url.yslow2s.last.o, td.url}</td></tr>"}
     top_ten_slowest << "</table>"   
   end
